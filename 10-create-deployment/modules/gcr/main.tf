63e70f4345022a97cc20d242f92cd562bdf84c5a
uchidataku <bbb.rchp99@gmail.com>
2022-12-12 21:57:32 +0900

    fix: gcr

--- 10-create-deployment/modules/gcr/main.tf
+++ 10-create-deployment/modules/gcr/main.tf
@@ -8,6 +8,6 @@ terraform {
 
 
 data "google_container_registry_image" "app_image" {
-  name = "${var.project_name}-${var.environment}"
+  name = "${var.project_name}-${var.environment}/app"
   tag  = "latest"
 }
