60932ed2b6c8aea31fd934f97df8dd472b31dfba
uchidataku <bbb.rchp99@gmail.com>
2023-01-08 17:53:38 +0900

    fix: load_config_fileコメントアウト

--- 10-create-deployment/main.tf
+++ 10-create-deployment/main.tf
@@ -37,7 +37,7 @@ data "google_container_cluster" "primary" {
 }
 
 provider "kubernetes" {
-  load_config_file = false
+#  load_config_file = false
 
   host                   = data.google_container_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
